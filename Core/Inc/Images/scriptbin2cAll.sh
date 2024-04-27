6c86965d407b92964f28c06f101edf8a1cadf2f3
hugo <hugomiquel498@gmail.com>
2024-04-27 10:57:48 +0200

    modify scripts

--- Core/Inc/Images/scriptbin2cAll.sh
+++ Core/Inc/Images/scriptbin2cAll.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-if [ ! -d "image_h" ]; then
-	mkdir image_h
+if [ ! -d "images_h" ]; then
+	mkdir images_h
 fi
 
 # Chemin vers le dossier avec les images .bmp
-bmp_folder="../images_bmp_color"
+bmp_folder="images_bmp_color"
 
 for file in "$bmp_folder"/*.bmp; do
 	if [ -f "$file" ]; then
-		output_file="image_h/${file%.bmp}.h"
+		output_file="images_h/$(basename "$file" .bmp).h"
 		./bin2c "$file" "$output_file"
+
+		echo "Image traitée: $(basename "$file")"
 	fi
 done
